53b9feb4df32a4c3dd337dee9157f4cdafbaf812
searchme86 <searchme86@gmail.com>
2022-12-23 22:36:43 +0900

    ⚡️ login router path changed

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/User/Login/Login';
 import Layout from './assets/Layout';
+import Home from './pages/Home/Home';
 
 function Router() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route element={<Layout />}>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
       </Route>
     </Routes>
   );
