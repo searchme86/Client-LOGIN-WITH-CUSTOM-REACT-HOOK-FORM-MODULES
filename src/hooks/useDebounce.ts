d91ef957e40e7e03eaf8ada8fb887687cce83b08
searchme86 <searchme86@gmail.com>
2022-12-24 23:07:27 +0900

    🗑️ delete files

--- src/hooks/useDebounce.ts
+++
@@ -1,9 +0,0 @@
-export const useDebounce = (callback: (args: any) => void) => {
-  let timeOutID: number | undefined;
-
-  return (args: any) => {
-    clearTimeout(timeOutID);
-
-    setTimeout(() => callback(args), 1000);
-  };
-};
