eda2e8708507fe0c713d800c9bba839415f33339
searchme86 <searchme86@gmail.com>
2022-12-26 15:20:49 +0900

    🔇 Remove txt log

--- src/hooks/useHookFormType.tsx
+++ src/hooks/useHookFormType.tsx
@@ -1,3 +1,15 @@
+/**
+ * useForm : we import useForm hook
+ * UseFormProps : typescript types of useForm props
+ * UseFormReturn: return type of useForm hook
+ * FieldValues : typescript type of form's field values
+ * SubmitHandler : type of submit handler event
+ * FormPropsWithZod : additional option that would be our zod schema.
+ * FormProps :
+ *  we omit the native `onSubmit` event in favor of `SubmitHandler` event
+ *  the beauty of this is, the values returned by the submit handler are fully typed
+ * */
+
 import React, { ComponentProps } from 'react';
 
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -20,7 +32,7 @@ interface FormPropsWithZod<T extends ZodSchema<any>>
 
 export interface FormProps<T extends FieldValues = any>
   extends Omit<ComponentProps<'form'>, 'onSubmit'> {
-  form: UseFormReturn<T>;
+  hookFormReturn: UseFormReturn<T>;
   onSubmit: SubmitHandler<T>;
 }
 
@@ -35,11 +47,11 @@ function useHookFormType() {
     });
   };
 
-  const form = ApplyingZodValidation({
+  const zodValueWithHookForm = ApplyingZodValidation({
     schema: signUpFormSchema,
   });
 
-  return { form };
+  return { zodValueWithHookForm };
 }
 
 export default useHookFormType;
