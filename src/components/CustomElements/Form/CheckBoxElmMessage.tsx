0882929f557882e076050eb254d0e7536c25c462
searchme86 <searchme86@gmail.com>
2022-12-27 13:42:58 +0900

    ⚡️ new custom Elm: checkboxResponseMessage

---
+++ src/components/CustomElements/Form/CheckBoxElmMessage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useFormContext } from 'react-hook-form';
+
+function CheckBoxElmMessage() {
+  const { getValues } = useFormContext();
+  const data = getValues(); //watch() as FormSchemaType
+  return data.accept ? <p>Thank you for accepting the agreements</p> : null;
+}
+
+export default CheckBoxElmMessage;
--- src/pages/User/Login/hooks/useLoginFormAction.tsx
+++ src/pages/User/Login/hooks/useLoginFormAction.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import { FormSchemaType } from '../LoginUtils/LoginFormType';
-import { SubmitHandler, useFormContext } from 'react-hook-form';
+import { SubmitHandler } from 'react-hook-form';
 
 function useLoginFormAction() {
-  const AgreementConfirmation = () => {
-    const { getValues } = useFormContext();
-    const data = getValues(); //watch() as FormSchemaType
-    return data.accept ? <p>Thank you for accepting the agreements</p> : null;
-  };
-
   const onSubmit: SubmitHandler<FormSchemaType> = async (data) => {
     console.log({ data });
     await new Promise((resolve) => {
@@ -19,7 +13,7 @@ function useLoginFormAction() {
     });
   };
 
-  return { AgreementConfirmation, onSubmit };
+  return { onSubmit };
 }
 
 export default useLoginFormAction;
