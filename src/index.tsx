f8823e44de08a9a0a46e8b470beff15549a764ab
searchme86 <searchme86@gmail.com>
2023-01-04 17:37:03 +0900

    💄 gloabal style location modified

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import GlobalStyle from './assets/styles/GlobalStyle';
+import GlobalStyle from './assets/styles/common/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
