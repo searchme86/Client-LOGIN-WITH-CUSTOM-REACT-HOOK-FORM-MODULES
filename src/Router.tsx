30d14a33f0af459d10d60b610b54743eeea2a647
searchme86 <searchme86@gmail.com>
2023-01-05 18:12:23 +0900

    🔥 router configured

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,22 +1,15 @@
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from './assets/Layout';
 import Home from './pages/Home/Home';
-import Login from './pages/User/Login';
+import Login from './pages/User/Login/Login';
 import Register from './pages/User/Register/Register';
-
-// import NewAppSignup from './Test/NewAppSignup';
-import NewAppSignup from './Test/NewAppSignup';
-// import Version03Upload from '../backups/Tuploader/Version03Upload';
 
 function Router() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/test" element={<NewAppSignup />} />
-
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
       </Route>
