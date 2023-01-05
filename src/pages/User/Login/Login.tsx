0ff669a44d49efa85e7557dd405481e9fcb923ce
searchme86 <searchme86@gmail.com>
2023-01-05 18:12:06 +0900

    🚚 rename filename

---
+++ src/pages/User/Login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginBackground from './LoginView/LoginBackground';
+import LoginForm from './Form/LoginForm';
+
+function Login() {
+  return (
+    <LoginBackground>
+      <LoginForm />
+    </LoginBackground>
+  );
+}
+
+export default Login;
