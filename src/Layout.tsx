6faaf120cc94a1a3523794b9264b832e5300f3a9
searchme86 <searchme86@gmail.com>
2023-01-16 13:05:25 +0900

    ➕ add absolute path to assets

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router';
 import Footer from '@components/Design/Footer/Footer';
 import Navbar from '@components/Design/Navbar/Navbar';
 
-import { MainContainer } from './assets/styles/common/PageLayout.style';
+import { MainContainer } from '@assets/styles/common/PageLayout.style';
 
 function Layout() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { StateContextProvider } from './context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import GlobalStyle from './assets/styles/common/GlobalStyle';
+import GlobalStyle from '@assets/styles/common/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
