9f9208126b3708d0286d02b776ff0fa0764ec68e
searchme86 <searchme86@gmail.com>
2023-01-26 13:32:18 +0900

    ⚡️ change RestFull action into get

--- src/pages/User/Login/Utils/LoginApi.ts
+++ src/pages/User/Login/Utils/LoginApi.ts
@@ -11,6 +11,9 @@ export const LoginUserApi = async (LoginInput: ILoginInfo) => {
 };
 
 export const LogoutUserApi = async () => {
-  const response = await axios.post('/logout', { withCredentials: true });
+  const response = await axios.get('/logout', {
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true,
+  });
   return response.data;
 };
