2a6797d4a0a0b6607a8c5202f3badcb0feb7c3b0
searchme86 <searchme86@gmail.com>
2023-01-26 16:55:42 +0900

    ⚰️ remove a code not to be used

--- src/components/styles/Button.style.ts
+++ src/components/styles/Button.style.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import { StyleProps } from './common/style.type';
 
-export const ButtonWrapper = styled.div``;
-
 export const Button = styled.button<StyleProps>`
   display: ${({ display }) => display};
   width: ${({ width }) => width};
