f45dcf8bcdc7f8b360fe613b64b3749a035fc50a
searchme86 <searchme86@gmail.com>
2023-01-26 15:52:42 +0900

    💄add indent in line with codes

--- src/components/styles/common/GlobalStyle.ts
+++ src/components/styles/common/GlobalStyle.ts
@@ -6,9 +6,9 @@ const GlobalStyle = createGlobalStyle`
         font-family:'Noto Sans KR', 'Roboto', 'Roboto Mono', sans-serif, monospace;
         font-size:16px;
     }
-    a{ text-decoration:none;}
-    ul, li{ list-style:none;}
-    fieldset{border:0; font-size:0; margin:0; padding:0;}
+    a { text-decoration:none;}
+    ul, li { list-style:none;}
+    fieldset {border:0; font-size:0; margin:0; padding:0;}
     legend { border: 0;  padding: 0;  margin:0;}
     textarea { overflow: auto; }
     optgroup { font-weight: bold; }
@@ -16,26 +16,24 @@ const GlobalStyle = createGlobalStyle`
     td, th { padding: 0; }
     main,nav,footer{display:block; width:100%;}
     button{ text-transform:none; border:0; outline:none; cursor:pointer;}
-}
-
-input[type='number']::-webkit-inner-spin-button,
-input[type='number']::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-       -moz-appearance: none;
-            appearance: none;
-}
-
-input::-ms-clear { display: none; }
-
-select {
-    -webkit-appearance: none;
-       -moz-appearance: none;
-            appearance: none;
-}
-
-select::-ms-expand { display: none; }
 
 
+    input[type='number']::-webkit-inner-spin-button,
+    input[type='number']::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+    }
+
+    input::-ms-clear { display: none; }
+
+    select {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+    }
+
+    select::-ms-expand { display: none; }
 
 `;
 
