0997cb5fc67bee7cfc2f1270f8b46eec1548ccca
searchme86 <searchme86@gmail.com>
2023-01-27 16:13:16 +0900

    ⚡️ add console log to check a value

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { PageContainer } from '@components/styles/common/pageLayout.style';
+import { useStateContext } from '@context/index';
 
 function Home() {
+  const {
+    contextValue: { authUser },
+  } = useStateContext();
+
+  console.log('authUser', authUser);
+
   return (
     <PageContainer>
       <p>메인 페이지 영역입니다.</p>
